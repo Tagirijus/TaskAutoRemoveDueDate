1bbccfbca12d3b93b85c41699c89e7814a364edb
Manuel <info@tagirijus.de>
2023-06-06 12:10:40 +0200

    First version

---
+++ Plugin.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Kanboard\Plugin\TaskAutoRemoveDueDate;
+
+use Kanboard\Core\Plugin\Base;
+use Kanboard\Core\Translator;
+use Kanboard\Plugin\TaskAutoRemoveDueDate\Action\TaskRemoveDueDate;
+
+
+class Plugin extends Base
+{
+    public function initialize()
+    {
+        $this->actionManager->register(new TaskRemoveDueDate($this->container));
+    }
+
+    public function onStartup()
+    {
+        Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
+    }
+
+    public function getPluginName()
+    {
+        // Plugin Name MUST be identical to namespace for Plugin Directory to detect updated versions
+        // Do not translate the plugin name here
+        return 'TaskAutoRemoveDueDate';
+    }
+
+    public function getPluginDescription()
+    {
+        return t('This plugin adds an automatic action to remove due date, if a task is moved to a certain column.');
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'Tagirijus';
+    }
+
+    public function getPluginVersion()
+    {
+        return '1.0.0';
+    }
+
+    public function getCompatibleVersion()
+    {
+        // Examples:
+        // >=1.0.37
+        // <1.0.37
+        // <=1.0.37
+        return '>=1.2.30';
+    }
+
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/Tagirijus/TaskAutoRemoveDueDate';
+    }
+}
---
+++ README.md
@@ -0,0 +1,41 @@
+# TaskAutoRemoveDueDate
+
+#### _Plugin for [Kanboard](https://github.com/fguillot/kanboard "Kanboard - Kanban Project Management Software")_
+
+This plugin adds the automatic action for removing the due date of a task, if it is being moved to a certain column.
+
+
+Compatibility
+-------------
+
+- Requires [Kanboard](https://github.com/fguillot/kanboard "Kanboard - Kanban Project Management Software") ≥`1.2.30`
+
+#### Other Plugins & Action Plugins
+- _No known issues_
+#### Core Files & Templates
+- _No database changes_
+
+
+Changelog
+---------
+
+Read the full [**Changelog**](../master/changelog.md "See changes")
+ 
+
+Installation
+------------
+
+1. Go into Kanboards `plugins/` folder
+2. `git clone https://github.com/Tagirijus/TaskAutoRemoveDueDate`
+
+
+Authors & Contributors
+----------------------
+
+- [@Tagirijus](https://github.com/Tagirijus) - Author
+- _Contributors welcome_
+
+
+License
+-------
+- This project is distributed under the [MIT License](../master/LICENSE "Read The MIT license")
